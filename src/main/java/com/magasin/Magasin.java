5f0ee6f8fd4b280a93239e55f75f8a2b55a0f737
Jean-Paul LEMONIAS <jean-paul.lemonias@Digital-Grenoble.local>
2022-12-20 14:57:08 +0100

    refacto Magasin.java

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -13,6 +13,50 @@ class Magasin {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+
+            if (!items[i].name.equals("Kryptonite")) {
+
+                if (items[i].quality < 50) {
+
+                    if (items[i].name.equals("Comté")) {
+                        items[i].quality = items[i].quality + 1;
+                        if (items[i].sellIn < 1) {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                    }
+
+                    else if (items[i].name.equals("Pass VIP Concert")) {
+                        if (items[i].sellIn < 6) {
+                            items[i].quality = items[i].quality + 3;
+                        } else if (items[i].sellIn < 11) {
+                            items[i].quality = items[i].quality + 2;
+                        } else {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                        if (items[i].quality > 50) {
+                            items[i].quality = 50;
+                        }
+                        if (items[i].sellIn < 1) {
+                            items[i].quality = 0;
+                        }
+                    }
+
+                    else {
+                        if (items[i].quality > 0) {
+                            items[i].quality = items[i].quality - 1;
+                        }
+                    }
+                }
+
+                items[i].sellIn = items[i].sellIn - 1;
+                if (items[i].sellIn < 0 && items[i].quality > 0 && !items[i].name.equals("Comté") ) {
+                    items[i].quality --;
+                }
+            }
+
+        }
+
+        /*for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Comté")
                     && !items[i].name.equals("Pass VIP Concert")) {
                 if (items[i].quality > 0) {
@@ -68,6 +112,6 @@ class Magasin {
                     }
                 }
             }
-        }
+        }*/
     }
 }
